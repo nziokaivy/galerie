e5cf63e4bdd1d4d3572e928276b06daaf0eb27b6
nziokaivy <nziokaivy@gmail.com>
2019-02-27 14:46:30 +0300

    add image,category and location models and add them to database

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.TextField()
+
+class Category(models.Model):
+    name = models.TextField()    
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.TextField()
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
