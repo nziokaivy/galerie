0289f96a3222da71b5c1a881cdc19c9687cb8782
nziokaivy <nziokaivy@gmail.com>
2019-02-27 16:01:40 +0300

    Add get image function

--- galerie/urls.py
+++ galerie/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'',include('photo.urls'))
 ]
--- photo/models.py
+++ photo/models.py
@@ -3,8 +3,18 @@ from django.db import models
 class Location(models.Model):
     name = models.TextField()
 
+
+    def __str__(self):
+       return self.name
+
 class Category(models.Model):
-    name = models.TextField()    
+    name = models.TextField()  
+
+   
+
+    def __str__(self):
+       return self.name
+  
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
@@ -13,4 +23,12 @@ class Image(models.Model):
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def get_image(cls):
+       return cls.objects.all()
+
+  
+    def __str__(self):
+       return self.name
 
