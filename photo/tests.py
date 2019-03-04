e20148f13980d2ee551ce10e34f994e0a7415f7e
nziokaivy <nziokaivy@gmail.com>
2019-03-04 15:01:45 +0300

    add copy link functionality

--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Image, Location, Category
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        africa = Location(name = "Africa")
+        food = Category(name = "food")
+        self.photo = Image(image ='images/landing.jpeg', name ='photograph image', description ='Photography is fun', location ='africa', Category ='people')
+ 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo,Image))
